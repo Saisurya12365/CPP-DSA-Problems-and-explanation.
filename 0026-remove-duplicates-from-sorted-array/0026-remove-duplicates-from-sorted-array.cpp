34e1815d21349bd9ba255af193f24902e25ab3f8
Sai Surya Bendi <bendisaisurya@gmail.com>
2026-07-30 14:21:12 +0530

    Time: 2 ms (13.42%), Space: 22.7 MB (53.85%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,17 +1,31 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-
-        for (int i = 0; i < nums.size() - 1; ) {
-
-            if (nums[i] == nums[i + 1]) {
-                nums.erase(nums.begin() + i + 1);
-            }
-            else {
-                i++;
+        
+        int k=1;
+        for(int i=1;i<nums.size();i++){
+            if(nums[i]!=nums[k-1]){
+                nums[k]=nums[i];
+                k++;
             }
         }
-
-        return nums.size();
+        return k;
     }
 };
+// class Solution {
+// public:
+//     int removeDuplicates(vector<int>& nums) {
+
+//         for (int i = 0; i < nums.size() - 1; ) {
+
+//             if (nums[i] == nums[i + 1]) {
+//                 nums.erase(nums.begin() + i + 1);
+//             }
+//             else {
+//                 i++;
+//             }
+//         }
+
+//         return nums.size();
+//     }
+// };
